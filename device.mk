9bd7e8506d9d00a86a0a2b7d01af07ae85b03f98
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2018-06-26 20:27:27 +0530

    zenfone3: Build some display hals
    * Drop unused color interface.

--- device.mk
+++ device.mk
@@ -220,7 +220,8 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml \
     libgenlock \
-    libdisplayconfig
+    libdisplayconfig \
+    libqdMetaData.system
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
@@ -230,8 +231,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    vendor.display.color@1.0-service \
-    vendor.display.color@1.0-impl
+    vendor.display.config@1.0 \
+    vendor.display.config@1.0_vendor
 
 # Connectivity Engine support
 PRODUCT_PACKAGES += \
