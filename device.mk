c6fa4c8477d5c431f48bb69770e1061435ad5c56
makorn645 <makornthawateugeneemery@gmail.com>
2019-05-10 13:46:46 +0530

    [debug] zenfone3: build with loggy

--- device.mk
+++ device.mk
@@ -308,6 +308,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Loggy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/loggy.sh:$(TARGET_COPY_OUT_VENDOR)/bin/loggy.sh
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
---
+++ rootdir/etc/loggy.sh
@@ -0,0 +1,3 @@
+_date=`date +%F_%H-%M-%S`
+logcat -b all -v time -f  /cache/logcat_${_date}.txt &
+cat /proc/kmsg > /cache/kmsg_${_date}.txt
