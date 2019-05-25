bc2ddccac8d4c81296a2e1046d9eb63e6b0c2cac
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2019-05-25 07:56:59 +0000

    zenfone3: Build libnl for WFD

--- device.mk
+++ device.mk
@@ -321,7 +321,8 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libOmxVidcCommon \
     libstagefrighthw \
-    libmm-omxcore
+    libmm-omxcore \
+    libnl
 
 PRODUCT_PACKAGES += \
     android.hardware.media@1.0 \
